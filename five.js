1467ffe242d4785f4411c594cc7d9d47bcd7556a
Hack Reactor Students <github@hackreactor.com>
2013-08-14 21:17:39 -0700

    Improved Error functionality.

--- five.js
+++ five.js
@@ -1,5 +1,13 @@
 Fivejs = {};
 
 Fivejs.five = function(num) {
-  return (typeof num === 'number') ? num*5 : console.log("eeerrrrmaahgaaawd", Fivejs.five(num));
+  try {
+    if (typeof num !== 'number') {
+      throw "invalid input to FiveJs.five() " + num + ' is not a mnumber.';
+    } else {
+      return num*5;
+    }
+  } catch (err) {
+    console.log('Error: ' + err);
+  }
 };
